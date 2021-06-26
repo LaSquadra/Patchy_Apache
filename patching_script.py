bfbb4c961538b6227e2fb9d84fbd5f7964dea032
LaSquadra <rkroetch@vt.edu>
2021-06-26 10:12:41 -0400

    Update patching_script.py

--- patching_script.py
+++ patching_script.py
@@ -1,1 +1,5 @@
 #Psudo code for now?
+
+#check the host machine for current version of Apache
+#check the host machine for vulnerabilities
+#check for the current version of Apache: https://httpd.apache.org/download.cgi
