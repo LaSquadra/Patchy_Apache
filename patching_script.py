9bf2a9aad66472aef0cac11943710f6f230b9d04
LaSquadra <77401166+LaSquadra@users.noreply.github.com>
2021-06-30 10:40:40 -0400

    Update patching_script.py

--- patching_script.py
+++ patching_script.py
@@ -9,30 +9,26 @@ from scapy.all import *
 #check for the current version of Apache: https://httpd.apache.org/download.cgi
 def check_current_version():
     installed_apache_version=subprocess.run(["apache2", "-v"], stdout=subprocess.PIPE, text=True)
-    return installed_apache_version.stdout
+    formatted_installed_version=""
+    for character in installed_apache_version.stdout:
+        formatted_installed_version+=character
+    formatted_installed_version=formatted_installed_version[:29]
+    return formatted_installed_version
 
 def check_newest_version():
-    newest_apache_version=urllib.request.urlopen("https://httpd.apache.org/download.cgi")
-    read_url=newest_apache_version.read()
+    getting_newest_version=urllib.request.urlopen("https://httpd.apache.org/download.cgi")
+    read_url=getting_newest_version.read()
     split_url=read_url.split()
     line_counter=0
     for line in split_url:
         line_counter+=1
         if line_counter==461:
             new_version=line.decode("utf-8")
-        if line_counter==463:
-            build_date=line.decode("utf-8")
-            #print(line_counter,line)
-    server_version=("Server version: A"+new_version[8:13]+"/"+new_version[17:23])
-    server_build_date=("Server built:   "+build_date[:10])
-    print(server_version + '\n' + server_build_date)
-    #print("Result code: " + str(newest_apache_version.getcode()))
-    #print(read_url)
-    return newest_apache_version
+    newest_server_version=("Server version: A"+new_version[8:13]+"/"+new_version[17:23])
+    return newest_server_version
 
-def version_comparison():    
+def version_comparison(installed_apache_version,newest_apache_version):    
     if installed_apache_version!=newest_apache_version:
-        print(installed_apache_version)
         print("You do not have the current version of Apache!")
         return False
     else:
@@ -44,7 +40,10 @@ def update_current_version():
 
 
 if __name__=="__main__":
-    print(check_current_version().strip())
-    check_newest_version()
+    #print(check_current_version().strip())
+    #print(check_newest_version().strip())
+    version_comparison(check_current_version(), check_newest_version())
     #if version_comparison()==False:
-    #    update_current_version()
+    #     update_current_version()
+    #else:
+    #    print("You are running the newest version of Apache.)
