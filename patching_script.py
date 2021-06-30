2c31b25bc712ddbbd132eb7efe66a4a75e4182ae
LaSquadra <77401166+LaSquadra@users.noreply.github.com>
2021-06-30 06:33:35 -0400

    Update patching_script.py

--- patching_script.py
+++ patching_script.py
@@ -8,11 +8,12 @@ from scapy.all import *
 #check the Apache for vulnerabilities
 #check for the current version of Apache: https://httpd.apache.org/download.cgi
 def check_current_version():
-    installed_apache_version=subprocess.run(["apache2", "-v"])
-    return installed_apache_version
+    installed_apache_version=subprocess.run(["apache2", "-v"], stdout=subprocess.PIPE, text=True)
+    return installed_apache_version.stdout
 
 def check_newest_version():
     newest_apache_version=urllib.request.urlopen("https://httpd.apache.org/download.cgi")
+
     print("Result code: " + str(newest_apache_version.getcode()))
     return newest_apache_version
 
@@ -20,13 +21,19 @@ def version_comparison():
     if installed_apache_version!=newest_apache_version:
         print(installed_apache_version)
         print("You do not have the current version of Apache!")
+        return False
     else:
         print("You have the current verion of Apache installed.")
+        return True
 
+def update_current_version():
+    print("This section is still under development")
 
 
 if __name__=="__main__":
-    check_current_version()
-    check_newest_version()
-    #version_comparison()
+    print(check_current_version())
+    print(check_newest_version())
+    #if version_comparison()==False:
+    #    update_current_version()
+
                            
