9af5440dfe5c2e409b311ccaed3a7244a919b87a
LaSquadra <77401166+LaSquadra@users.noreply.github.com>
2021-06-29 21:32:18 -0400

    Update patching_script.py

--- patching_script.py
+++ patching_script.py
@@ -1,5 +1,26 @@
-#Psudo code for now?
+! /usr/bin/python3
+import sys, subprocess
+from scapy.all import * 
 
+#Psudo code for now?
+#use argparse to get config file locations and IP addresses and such
 #check the host machine for current version of Apache
-#check the host machine for vulnerabilities
+#check the Apache for vulnerabilities
 #check for the current version of Apache: https://httpd.apache.org/download.cgi
+def check_current_version():
+    installed_apache_version=subprocess.run("apache2 -v")
+    newest_apache_version="Server version: Apache/2.4.46 (Debian)"
+def check_newest_version()
+    
+def version_comparison()    
+    if installed_apache_version!=newest_apache_version:
+        print(installed_apache_version)
+        print("You do not have the current version of Apache!")
+    else:
+        print("You have the current verion of Apache installed.")
+
+
+
+if __name__=="__main__":
+    quick_patch()
+                     
